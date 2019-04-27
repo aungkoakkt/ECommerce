7bf2b0cc156e50577990e90d0c08fe1d11d5ed3d
AungKo <aungko.akkt@gmail.com>
2019-04-27 22:31:28 +0630

    Add foreign constraint in CategoryProduct Entity

--- ECommerce(Kotlin)/app/src/main/java/com/akkt/ecommerce/persistence/entities/CategoryProduct.kt
+++ ECommerce(Kotlin)/app/src/main/java/com/akkt/ecommerce/persistence/entities/CategoryProduct.kt
@@ -2,13 +2,25 @@ package com.akkt.ecommerce.persistence.entities
 
 import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
+import android.arch.persistence.room.ForeignKey
+import com.akkt.ecommerce.data.vos.CategoryVO
+import com.akkt.ecommerce.data.vos.ProductVO
 
 /**
  *Created by Aung Ko Ko Thet on 4/27/19
  */
 @Entity(
     tableName = "category_product",
-    primaryKeys = ["product_id", "category_id"]
+    primaryKeys = ["product_id", "category_id"],
+    foreignKeys = [
+        ForeignKey(
+            entity = ProductVO::class,
+            parentColumns = ["product_id"], childColumns = ["product_id"]
+        ),
+        ForeignKey(
+            entity = CategoryVO::class,
+            parentColumns = ["category_id"], childColumns = ["category_id"]
+        )]
 )
 data class CategoryProduct(
 
