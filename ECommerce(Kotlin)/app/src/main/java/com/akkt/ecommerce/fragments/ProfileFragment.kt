efb8ce3894c9796ce1a1fec564d09f2da4a41bf0
AungKo <aungko.akkt@gmail.com>
2019-04-30 16:06:20 +0630

    Add User Profile Page ( Incomplete Collection & History)

--- ECommerce(Kotlin)/app/src/main/java/com/akkt/ecommerce/fragments/ProfileFragment.kt
+++ ECommerce(Kotlin)/app/src/main/java/com/akkt/ecommerce/fragments/ProfileFragment.kt
@@ -6,14 +6,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.akkt.ecommerce.R
+import com.akkt.ecommerce.data.models.UserModel
+import com.akkt.ecommerce.data.models.UserModelImpl
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment : Fragment() {
 
+    private val mUserModel: UserModel
+
+    init {
+        mUserModel = UserModelImpl
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val userInfo = mUserModel.getUserInformation()
+
+        Glide.with(this).load(userInfo.profileImage).error(R.drawable.if_reindeer).into(ivFragmentProfile)
+        tvFragmentProfileName.text=userInfo.name
+        tvFragmentProfileAddress.text=userInfo.address
+        tvFragmentProfileWallet.text=userInfo.wallet
+    }
 
 }
